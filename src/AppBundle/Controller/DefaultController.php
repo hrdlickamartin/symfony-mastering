59db6962137c88917c2b657acae313c15e2a47e0
Lukas Hrdlicka <hrdlickal@lmc.eu>
2017-10-23 13:37:10 +0200

    Updated default controler

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,8 +13,8 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $users = $this->getDoctrine()->getRepository('AppBundle:User')->findAllUsersAsArray();
-        var_dump($users);die;
+        // $users = $this->getDoctrine()->getRepository('AppBundle:User')->findAllUsersAsArray();
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
